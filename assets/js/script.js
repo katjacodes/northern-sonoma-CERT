7af9a49bee471244e54d73a14ff145442c752f64
katjacodes <katjacodes@gmail.com>
2021-07-07 01:33:43 +0000

    modify js code for image gallery to work and add css code for image gallery to be scrollable

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,19 +15,15 @@ function SubForm (){
     });
 }
        
-       
-/*------------ GALLERY PAGES ------------*/
 
-/* credit: code for image gallery taken from https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp and edited slightly to fit project needs */
+/*------------ GALLERY PAGE ------------*/
+
+/* credit: code for image gallery taken from https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp and edited to fit project needs */
 function myFunction(imgs) {
     // Get the expanded image
     var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
     // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
     // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
   }
